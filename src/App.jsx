c76531204130e9cb94f0b92ec7dfa15b1157bc4d
nmackee <70114875+nmackee@users.noreply.github.com>
2022-03-01 23:43:42 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import "./app.css";
+import { Users } from "./users";
+
+export default function App() {
+  const [query, setQuery] = useState("");
+  console.log(
+    Users.filter((user) => user.first_name.toLowerCase().includes("fe"))
+  );
+  return (
+    <div className="App">
+      <input
+        type="text"
+        placeholder="Search..."
+        className="search"
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <ul className="list">
+        {Users.filter((user) =>
+          user.first_name.toLowerCase().includes(query)
+        ).map((user) => (
+          <li key={user.id} className="listItem">
+            {user.first_name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
