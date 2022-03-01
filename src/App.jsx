f6a5757069cd622b2a3f1325f52e0f27bd137d8b
nmackee <70114875+nmackee@users.noreply.github.com>
2022-03-02 01:44:29 +0900

    implement multi-search

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import "./app.css";
 import { Users } from "./users";
+import { Table } from "./Table";
 
 export default function App() {
   const [query, setQuery] = useState("");
-  console.log(
-    Users.filter((user) => user.first_name.toLowerCase().includes("fe"))
-  );
+
+  const keys = ["first_name", "last_name", "email"];
+  console.log(Users[0]["first_name"]);
+
+  const search = (data) => {
+    return data.filter((item) =>
+      // item.first_name.toLowerCase().includes(query) ||
+      // item.last_name.toLowerCase().includes(query) ||
+      // item.email.toLowerCase().includes(query)
+
+      keys.some((key) => item[key].toLowerCase().includes(query))
+    );
+  };
   return (
     <div className="App">
       <input
@@ -25,6 +36,8 @@ export default function App() {
           </li>
         ))}
       </ul> */}
+
+      <Table data={search(Users)} />
     </div>
   );
 }
